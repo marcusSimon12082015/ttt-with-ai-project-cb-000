c917a956d1031729e6bc37e45aaca5451f2cc437
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-13 18:56:57 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,5 @@
 class Board
   attr_accessor :cells
-  WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [6,4,2]
-]
   def initialize
     @cells = Array.new(9," ")
   end
