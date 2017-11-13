d5c9e0506927a232857fd2e6b2750342be03d427
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-13 20:33:53 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -58,19 +58,19 @@ class Game
           if type_of_game == "1"
             loop do
             end_loop = false
-              puts "Who should go first? 1 - Computer |-|-| 2 - Human?"
-                  go_first = gets.strip
-                  if ["1","2"].detect{|element| element == go_first} != nil
-                    if go_first == "1"
-                      game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
-                      end_loop = true
-                    else
-                      game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
-                      end_loop = true
-                    end
+            puts "Who should go first? 1 - Computer |-|-| 2 - Human?"
+                go_first = gets.strip
+                if ["1","2"].detect{|element| element == go_first} != nil
+                  if go_first == "1"
+                    game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
+                    end_loop = true
+                  else
+                    game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
+                    end_loop = true
                   end
-                  break if end_loop
                 end
+                break if end_loop
+              end
           end
           game = Game.new if type_of_game == "2"
           game.play
